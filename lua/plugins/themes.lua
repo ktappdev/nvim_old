f11a6a4c14f0bcaad9c19b2d44221272a51bf0a3
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2023-08-05 13:29:59 -0400

    init

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,12 @@
+return {
+  { "EdenEast/nightfox.nvim" },
+  { "glepnir/zephyr-nvim" },
+  { "glepnir/oceanic-material" },
+  { "glepnir/dashboard-nvim" },
+  { "navarasu/onedark.nvim" },
+  { "shaunsingh/nord.nvim" },
+  { "sainnhe/sonokai" },
+  { "sainnhe/gruvbox-material" },
+  { "sainnhe/edge" },
+  { "sainnhe/everforest" },
+}
