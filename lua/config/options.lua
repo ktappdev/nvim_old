43ff6c20f56c3af845f55e7cb24eb823939145b4
Ken Taylor <kentaylosappdev@gmail.com>
2023-11-30 12:38:44 -0400

    codieum fully sorted out

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
 vim.opt.winbar = "%=%m %f"
+vim.opt.pumblend = 0 -- makes the popup suggestions background not see through
+
+-- ``local float = { focusable = true, style = "minimal", border = "rounded" }
+-- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, float)
+-- vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, float)
