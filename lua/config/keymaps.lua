51db2054b16b7cecdf6569c3221793ad561585d9
Ken Taylor <kentaylosappdev@gmail.com>
2024-01-07 13:34:38 -0400

    nothing new just clean up

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,14 +1,11 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
 local opts = { noremap = true, silent = true }
-local keymap = vim.api.nvim_set_keymap
--- vim.g.maplocalleader("\\")
+local keymap = vim.keymap.set
 local harpoon = require("harpoon")
 
 -- REQUIRED
-harpoon:setup()
+harpoon:setup({})
 -- REQUIRED
+--
 require("toggleterm").setup({ -- size can be a number or function which is passed the current terminal
   function(term)
     if term.direction == "horizontal" then
@@ -20,7 +17,7 @@ require("toggleterm").setup({ -- size can be a number or function which is passe
   open_mapping = [[<c-\>]],
 })
 
-vim.keymap.set(
+keymap(
   "n",
   "<leader>sx",
   require("telescope.builtin").resume,
@@ -49,15 +46,24 @@ keymap("n", "<C-Up>", ":resize -2<cr>", opts)
 keymap("n", "<C-Down>", ":resize +2<cr>", opts)
 keymap("n", "<C-Left>", ":vertical resize -2<cr>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<cr>", opts)
+
 -- Harpoon
-vim.keymap.set("n", "<leader>a", function()
+keymap("n", "<leader>a", function()
   harpoon:list():append()
 end, { noremap = true, silent = true, desc = "Add file to Harpoon" })
-vim.keymap.set("n", "<leader>h", function()
+
+keymap("n", "<leader>h", function()
   harpoon.ui:toggle_quick_menu(harpoon:list())
 end, { noremap = true, silent = true, desc = "Show Harpoon list" })
+
 -- clear all console.logs from file
-keymap("n", "<leader>cL", [[:%s/console\.log([^)]*);//g<CR>]], { noremap = true, silent = true })
+keymap(
+  "n",
+  "<leader>cL",
+  [[:%s/console\.log([^)]*);//g<CR>]],
+  { noremap = true, silent = true, desc = "Clear all console logs" }
+)
+-- sort tailwind classes
 keymap("n", "<leader>ct", ":TailwindSort<CR>", { noremap = true, silent = true, desc = "Tailwind Sort" })
 keymap(
   "n",
@@ -65,40 +71,30 @@ keymap(
   ":TailwindSortOnSaveToggle<CR>",
   { noremap = true, silent = true, desc = "Tailwind Sort on save toggle" }
 )
--- Map 'hh' to paste from yank register 0
--- keymap("n", "<localleader>d", '"_dd', { noremap = true, silent = true }) -- "_dw  -- in visual mode "_d
+
 keymap("n", "<leader>n", ":NnnPicker<cr>", opts)
 
--- Redo last selected option
-vim.api.nvim_set_keymap(
-  "n",
-  "<S-F6>",
-  "<cmd>CompilerStop<cr>" -- (Optional, to dispose all tasks before redo)
-    .. "<cmd>CompilerRedo<cr>",
-  { noremap = true, silent = true }
-)
-
 -- Zen Mode
-vim.api.nvim_set_keymap("n", "<leader>z", "<cmd>ZenMode<cr>", { noremap = true, silent = true })
+keymap("n", "<leader>z", "<cmd>ZenMode<cr>", { noremap = true, silent = true })
 
 -- Toggleterm keymaps
-vim.api.nvim_set_keymap(
+keymap(
   "n",
   "<leader>th",
-  ":ToggleTerm direction=horizontal<cr>",
+  ":ToggleTerm direction=horizontal size=60<cr>",
   { noremap = true, silent = true, desc = "Horivontal terminal" }
 )
-vim.api.nvim_set_keymap(
+keymap(
   "n",
   "<leader>tv",
   ":ToggleTerm direction=vertical size=60<cr>",
   { noremap = true, silent = true, desc = "Vertical terminal" }
 )
-vim.api.nvim_set_keymap(
+keymap(
   "n",
   "<leader>tf",
   ":ToggleTerm direction=float<cr>",
   { noremap = true, silent = true, desc = "Floating terminal" }
 )
-vim.api.nvim_set_keymap("n", "<leader>ta", ":ToggleTermToggleAll<cr>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>tn", ":ToggleTermSetName<cr>", { noremap = true, silent = true })
+keymap("n", "<leader>ta", ":ToggleTermToggleAll<cr>", { noremap = true, silent = true })
+keymap("n", "<leader>tn", ":ToggleTermSetName<cr>", { noremap = true, silent = true })
