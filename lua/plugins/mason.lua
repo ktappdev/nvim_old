92e2b1f08aca6b71bf0188109027ff186d983ea7
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2024-01-26 11:56:07 -0400

    added mason

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,13 @@
+-- this file will auto instal the main lsp stuff from mason.
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls",
+        "typescript-language-server",
+        "clangd",
+      },
+    },
+  },
+}
